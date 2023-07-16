ecae26da10c6f4b957dd6d194e8f8586d9ba03ba
ARBUCHELI <radio_seattle@hotmail.com>
2023-07-16 12:49:03 +0300

    feat: php strings numbers and variables -using variables

---
+++ index5.php
@@ -0,0 +1,8 @@
+<?php
+// Write your code below:
+$name = "Andres";
+$language = "PHP";
+
+echo "The student called ".$name . " ". "is learning ". $language;
+
+echo "\nThe student called ".$name . " ". "is learning ".$language;
