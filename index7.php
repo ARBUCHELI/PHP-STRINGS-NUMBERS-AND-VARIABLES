53521a1f4117076f276f4ec7964712fe7da491a0
ARBUCHELI <radio_seattle@hotmail.com>
2023-07-16 13:16:58 +0300

    feat: php strings numbers and variables -variable reassignment

---
+++ index7.php
@@ -0,0 +1,19 @@
+<?php
+  $movie = "___";
+// Add your code here:
+$movie = "E.T";
+
+$old_favorite = $movie;
+
+
+
+  echo "I'm a fickle person, my favorite movie used to be $movie.";
+  
+// Add a statement here:
+ $movie = "Braveheart"; 
+  
+  echo "\nBut now my favorite is $movie.";
+  
+// Add a statement below:
+echo "\n";
+echo "Anyway, my old favorite movie is still $old_favorite.";
